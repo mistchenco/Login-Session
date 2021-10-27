5eeeae9e50f979f573d7bb22c831b9508507cded
mistchenco <augusto.perceval@est.fi.uncoma.edu.ar>
2021-10-26 23:23:48 -0300

    comentarios en codigo

--- utiles/PHPMailer/enviaMail.php
+++ utiles/PHPMailer/enviaMail.php
@@ -1,15 +1,16 @@
 <?php
 // include_once '../estructura/cabecera.php';
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
 
 require 'Exception.php';
 require 'PHPMailer.php';
 require 'SMTP.php';
 
 /**
- * 
+ * Creamos la funcion enviarMail para que pueda ser utilizada en distintos momentos dependendiento de la implementacion
  */
 class enviarMail{
  
@@ -23,26 +24,25 @@ class enviarMail{
 
     private $mailFrom;
     private $mailSender;
-    function __construct()
-    {
-        $this->mail       = new PHPMailer(true);
-        $this->host       ="smtp.gmail.com";
-        $this->SMTPAuth   =true;
-        $this->Username   ="programacionweb73@gmail.com";
-        $this->Password   ="fai38493294";
-        $this->SMTPSecure ="TLS";
-        $this->Port       =587;
+    function __construct(){
+        $this->mail       = new PHPMailer(true);// Se crea la clase PHPMailer
+        $this->host       ="smtp.gmail.com"; //Configura el servidor smtp para enviar mail
+        $this->SMTPAuth   =true; //Habilita la autenticacion smtp
+        $this->Username   ="programacionweb73@gmail.com"; //Cuenta por la que se envia email
+        $this->Password   ="fai38493294"; //pass de la cuenta
+        $this->SMTPSecure ="TLS"; //El protocolo TLS (Transport Layer Security, seguridad de la capa de transporte)  
+        $this->Port       =587;// Puerto de salifa
         
-        $this->mailFrom   ="programacionweb73@gmail.com";
-        $this->mailSender ="Administracion de Sistemas";
+        $this->mailFrom   ="programacionweb73@gmail.com"; //Quien envia el correo
+        $this->mailSender ="Administracion de Sistemas";// a nombre de 
     }
 
     public function newEmail($mailFrom="", $mailSender="",  $mailFor="", $mailRecipientName="", $mailSubject="", $mailBody="")
     {
         try {
             //Server settings
-            $this->mail->SMTPDebug = false;
-            $this->mail->isSMTP();
+            $this->mail->SMTPDebug = false; //Da informacion en caso de no poder conectar al smtp
+            $this->mail->isSMTP();  //Envio mediante servidor SMTP
             $this->mail->Host       = $this->host;
             $this->mail->SMTPAuth   = $this->SMTPAuth;
             $this->mail->Username   = $this->Username;
@@ -60,8 +60,8 @@ class enviarMail{
        
 
             // Content
-            $this->mail->isHTML(true);                                  // Set email format to HTML
-            $this->mail->Subject = $mailSubject;
+            $this->mail->isHTML(true); // Enviamos en formato html
+            $this->mail->Subject = $mailSubject; 
             $this->mail->Body    = $mailBody;
             $this->mail->AltBody = 'Su gestor de correos no soporta HTML.';
 
